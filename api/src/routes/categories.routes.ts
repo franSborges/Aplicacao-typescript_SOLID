fea7f879fac1cc5f7e27bd617341a2b718286ca2
franSborges <borgesfran988@gmail.com>
2022-06-13 17:02:30 -0300

    :CHORE: dependency for uploading downloaded files

--- api/src/routes/categories.routes.ts
+++ api/src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-
+const upload = multer({
+  dest: "./tmp"
+})
 categoriesRoutes.post("/",  (req, res) => createCategoryController.handle(req, res));
 
 
 categoriesRoutes.get("/", (req, res) => listCategoriesController.handle(req, res));
 
+categoriesRoutes.post("/import", (req, res) => {
 
+})
 export { categoriesRoutes };
 
 
