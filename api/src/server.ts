bb098678bc4543f8cbb2ed0ff848b71459b32afa
franSborges <borgesfran988@gmail.com>
2022-06-14 21:16:37 -0300

    :CHORE: implementation of dependecies for api documentation

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+//const swaggerFile from "./swagger.json";
 
 const app = express();
+
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(express.json());
 app.use(router);
 
