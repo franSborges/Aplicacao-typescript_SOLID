995cac06801f3ec7d5dfe50758e2cb5adc66b874
franSborges <borgesfran988@gmail.com>
2022-10-14 15:59:37 -0300

    :feat: docker compose implemented

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
-import { router } from "./routes";
+import "./database";
 
 const app = express();
 
@@ -13,4 +14,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3000);
+app.listen(3333);
